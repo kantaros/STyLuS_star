21c19020933b60777acc1c4af097e4163e6e808c
yiannis <44352538+kantaros@users.noreply.github.com>
2021-08-16 15:53:04 -0400

    Add files via upload

---
+++ STyLuS_star.m
@@ -0,0 +1,3 @@
+[T, N, x0, phi, N_p, B1, AP, Termination, w] = DefineInputs(); 
+
+[BestPrefix, BestSuffix, ListOfPrefix, ListOfSuffix, wrongPre, wrongSuf] = MainSTyLuS_star(T, N, x0, N_p, B1, AP, Termination, w);
